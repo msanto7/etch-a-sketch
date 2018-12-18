7c12e98ac1a3c43b1b8c8ead0ac5eb94026422c6
MIKE <mike.santoro42@gmail.com>
2018-12-18 18:05:35 -0500

    grid setup (need to eliminate Jquery)

--- script.js
+++ script.js
@@ -5,12 +5,26 @@ function createGrid(numRowCol) {
 
     for (let rows = 0; rows < numRowCol; rows++) {
         for (let cols = 0; cols < numRowCol; cols++) {
-            $("grid-container").append("<div class=;grid-item'></div>");
+            $("grid-container").append("<div class='grid-item'></div>");
         }
     }
 
-    $(".grid-item").width(960/x);
-    $(".grid-item").height(960/x);
+    $(".grid-item").width(960/numRowCol);
+    $(".grid-item").height(960/numRowCol);
+
+} //end create function
+
+//*******************************************
 
 
-}
+
+//Driver 
+
+//this is a Jquery function to make this grid show up on load
+
+$(document).ready(function() {
+
+    createGrid(16);
+
+});
+
