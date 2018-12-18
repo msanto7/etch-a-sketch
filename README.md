fb68f2c2676373fcaf4c98856ccdc5e879c31887
MIKE <mike.santoro42@gmail.com>
2018-12-18 06:11:27 -0500

    initial grid method

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # etch-a-sketch
 This is an etch-a-sketch app built using HTML, CSS, and JavaScript
+
+Currently Hosted at https://msanto7.github.io/etch-a-sketch/
--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //js to create div grid
+
+
+function createGrid(numRowCol) {
+
+    for (let rows = 0; rows < numRowCol; rows++) {
+        for (let cols = 0; cols < numRowCol; cols++) {
+            $("grid-container").append("<div class=;grid-item'></div>");
+        }
+    }
+
+    $(".grid-item").width(960/x);
+    $(".grid-item").height(960/x);
+
+
+}
