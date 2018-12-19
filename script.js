a33e8a38d53b313a896bdf9963efb1d161dfe10d
MIKE <mike.santoro42@gmail.com>
2018-12-18 23:39:10 -0500

    createGrid working (eliminated Jquery)

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ function createGrid(numRowCol) {
 
     for (let rows = 0; rows < numRowCol; rows++) {
         for (let cols = 0; cols < numRowCol; cols++) {
-            $("grid-container").append("<div class='grid-item'></div>");
+            //$("grid-container").append("<div class='grid-item'></div>");
+            let newDiv = document.createElement("div");
+            newDiv.setAttribute("class", "grid-item");
+            //console.log(newDiv.className);
+            document.getElementsByClassName("grid-container")[0].appendChild(newDiv);
         }
     }
 
-    $(".grid-item").width(960/numRowCol);
-    $(".grid-item").height(960/numRowCol);
+    //(".grid-item").width(960/numRowCol);
+    //document.setAttribute(".grid-item").height(960/numRowCol);
 
 } //end create function
 
@@ -21,10 +25,11 @@ function createGrid(numRowCol) {
 //Driver 
 
 //this is a Jquery function to make this grid show up on load
-
+/* JQeuery Option
 $(document).ready(function() {
-
     createGrid(16);
-
 });
+*/
+
+document.body.onload = createGrid(16);
 
