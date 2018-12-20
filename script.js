bd7330b56d8845e6c4d91d9b2fb3944b538bb311
MIKE <mike.santoro42@gmail.com>
2018-12-19 21:24:52 -0500

    Reset working

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
 //js to create div grid
+let grid = document.getElementsByClassName("grid-container")[0];
+const container = document.querySelector(".grid-container");
 
 
-const container = document.querySelector('.grid-container');
+function createGrid(numRow, numCol) {
 
-
-function createGrid(numRowCol) {
-
-    for (let rows = 0; rows < numRowCol; rows++) {
-        for (let cols = 0; cols < numRowCol; cols++) {
+    for (let rows = 0; rows < numRow; rows++) {
+        for (let cols = 0; cols < numCol; cols++) {
             let newDiv = document.createElement("div");
             newDiv.setAttribute("class", "grid-item");
             document.getElementsByClassName("grid-container")[0].appendChild(newDiv);
         }
     }
+}
 
-    //(".grid-item").width(960/numRowCol);
-    //document.setAttribute(".grid-item").height(960/numRowCol);
-
-} //end create function
-
-//*******************************************
 //Mouse Over
-
-
-
-
 container.addEventListener('mousemove', (e) => {
-    e.target.style.background = 'brown';
+    e.target.style.background = 'black';
 });
 
-
-
-
-//checking to see if gitpages is working 
-
-
+//Reset Button
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", () => {
+    reset();
+    createGrid(60, 90);
+});
+function reset() {
+    while (grid.firstChild) grid.removeChild(grid.firstChild);
+    //createGrid(60,90);
+}
 
 //Driver 
-document.body.onload = createGrid(16);
+document.body.onload = createGrid(60, 90);
 
